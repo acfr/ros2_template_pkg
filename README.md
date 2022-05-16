d2d79f4fb99b635b9d9cab1c5471c59b63663e69
Jerome Justin <jclinton830@gmail.com>
2022-05-16 11:40:57 +1000

    package name change

---
+++ README.md
@@ -0,0 +1,6 @@
+##About
+
+A template for starting off a ros2 package.
+
+Check individual branches to see examples of simple nodes.
+
---
+++ src/ros2_template_node.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("This is a ros2_template_pkg package node\n");
+  return 0;
+}
