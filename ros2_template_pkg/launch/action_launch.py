d989be32e31de2474ad1c8cc853903b74ce46bca
Jerome Justin <jclinton830@gmail.com>
2022-06-06 15:39:52 +1000

    Added launch files to support new nodes

---
+++ ros2_template_pkg/launch/action_launch.py
@@ -0,0 +1,22 @@
+import platform
+
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+env_var_name = 'USER' if platform.system() != 'Windows' else 'USERNAME'
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'node_prefix',
+            default_value=[launch.substitutions.EnvironmentVariable(env_var_name), '_'],
+            description='Prefix for node names'),
+        launch_ros.actions.Node(
+            package='multi_node_launcher', executable='action_server', output='screen',
+            name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'action_server']),
+        launch_ros.actions.Node(
+            package='multi_node_launcher', executable='action_client', output='screen',
+            name=[launch.substitutions.LaunchConfiguration('node_prefix'), 'action_client'])         
+    ])
---
+++ ros2_template_pkg/launch/sequenced_launch.py
@@ -0,0 +1,12 @@
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, TimerAction
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        
+    ])
